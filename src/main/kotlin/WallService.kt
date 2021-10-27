5e1135aa3b1ad278202f9034ae1e171669e8fed9
NikitaZar <webbizon@ya.ru>
2021-10-27 09:21:31 +0300

    Fixed bug in post.Id preset, fixed bug in fun update, tests refactoring

--- src/main/kotlin/WallService.kt
+++ src/main/kotlin/WallService.kt
@@ -2,15 +2,20 @@ class WallService {
     private val posts = arrayListOf<Post>()
 
     fun add(post: Post): Post {
-        val newPost = post.copy(id = post.hashCode())
+        val newPost: Post = when (posts.isEmpty()) {
+            true -> post.copy(id = 1)
+            false -> post.copy(id = posts.last().id + 1)
+        }
+
         posts.add(newPost)
         return newPost
     }
 
     fun update(post: Post): Boolean {
-        for (p in posts) {
+        for ((i, p) in posts.withIndex()) {
             if (p.id == post.id) {
                 val newPost = post.copy(ownerId = p.ownerId, date = p.date)
+                posts[i] = newPost
                 return true
             }
         }
--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -3,21 +3,26 @@ import org.junit.Assert.*
 
 class WallServiceTest {
 
-    val wallService = WallService()
-
     @Test
     fun add() {
+        val wallService = WallService()
         var post = Post()
         post = wallService.add(post)
         assertNotEquals(post.id, 0)
     }
 
     @Test
-    fun update() {
+    fun update_foundPost() {
+        val wallService = WallService()
         var post = Post()
         post = wallService.add(post)
 
         assertTrue(wallService.update(post))
+    }
+
+    @Test
+    fun update_notFoundPost() {
+        val wallService = WallService()
         assertFalse(wallService.update(Post()))
     }
 }
