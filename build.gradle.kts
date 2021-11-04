4478565ee9b6d0abec50d3ddb0d547a3e5663a4f
NikitaZar <webbizon@ya.ru>
2021-11-04 22:37:14 +0300

    Task 8 done. Add jacoco report

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,10 @@ dependencies {
 }
 
 tasks.test {
-    useJUnit()
+    finalizedBy(tasks.jacocoTestReport) // report is always generated after tests run
+}
+tasks.jacocoTestReport {
+    dependsOn(tasks.test) // tests are required to run before generating the report
 }
 
 tasks.withType<KotlinCompile>() {
